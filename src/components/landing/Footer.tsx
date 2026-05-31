7b3c7a865db6f8750a9d8e397f71b5e64754f11a
savuremus17-collab <savu.remus17@gmail.com>
2026-05-31 22:24:48 +0300

    Update Footer.tsx

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -10,7 +10,7 @@ const footerLinks = [
       { name: "Features", href: "#features" },
       { name: "Pricing", href: "#pricing" },
       { name: "Demo", href: "#" },
-      { name: "Changelog", href: "#" },
+    
     ],
   },
   {
